1252ff18296a1f237e4ba43101bcdfcba1c7b321
joaogabriel1995 <joaogabrielrp@gmail.com>
2022-05-25 22:59:27 -0300

    update

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,9 +1,11 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
 import {
   CreateUserController,
   DeleteUserController,
   FindManyUserController,
-  UpdateUserController
+  UpdateUserController,
+  CreateProductController,
+  FindManyProductController
 } from '../controllers'
 
 const createUserController = new CreateUserController()
@@ -11,10 +13,15 @@ const findManyUserController = new FindManyUserController()
 const updateUserController = new UpdateUserController()
 const deleteUserController = new DeleteUserController()
 
+const createProductController = new CreateProductController()
+const findManyProductController = new FindManyProductController()
+
 const appRouter = Router()
 appRouter.post('/user', createUserController.handle)
 appRouter.get('/user', findManyUserController.handle)
 appRouter.put('/user', updateUserController.handle)
 appRouter.delete('/user/del=:id', deleteUserController.handle)
 
+appRouter.post('/product', createProductController.handle)
+appRouter.get('/product', findManyProductController.handle)
 export { appRouter }
